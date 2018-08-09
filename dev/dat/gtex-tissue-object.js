d98cd675f1ccca357191c8431293f8cc299aa7d9
Douglass Turner <douglass.turner@gmail.com>
2018-08-09 16:17:51 -0400

    bug fix. gtex.

---
+++ dev/dat/gtex-tissue-object.js
@@ -0,0 +1,16 @@
+let tissue_info_object =
+    {
+        "colorHex": "FF6600",
+        "colorRgb": "255,102,0",
+        "datasetId": "gtex_v7",
+        "eGeneCount": 11866,
+        "expressedGeneCount": 27945,
+        "hasEGenes": true,
+        "rnaSeqAndGenotypeSampleCount": 385,
+        "rnaSeqSampleCount": 442,
+        "tissueAbbrv": "ADPSBQ",
+        "tissueId": "Adipose_Subcutaneous",
+        "tissueName": "Adipose - Subcutaneous"
+    };
+
+

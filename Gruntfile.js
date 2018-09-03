a8fc5d60aa9b47101a929116fae9bf0c78f0c8cb
Jim Robinson <jrobinso@broadinstitute.org>
2018-09-02 22:17:14 -0700

    remove build artifacts Generate dist/index.html from index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 const webpackConfig = require('./webpack.config.js');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+
     grunt.initConfig({
         webpack:
             {
@@ -9,13 +10,35 @@ module.exports = function(grunt) {
                         stats: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
                     },
                 prod: webpackConfig,
-                dev: Object.assign({ watch: true }, webpackConfig)
+                dev: Object.assign({watch: true}, webpackConfig)
+            },
+
+        'string-replace': {
+            dist: {
+                files: {
+                    'dist/index.html': 'index.html'
+                },
+                options: {
+                    replacements: [
+                        {
+                            pattern: '<script type="module" src="js/app.js"></script>',
+                            replacement: ''
+                        },
+                        {
+                            pattern: '<!--bundle-->',
+                            replacement: '<script src="bundle.js"></script>'
+                        }]
+                }
             }
+        }
+
     });
 
     grunt.loadNpmTasks('grunt-webpack');
+    grunt.loadNpmTasks('grunt-string-replace');
 
     grunt.registerTask('default', 'Webpack rocks', ['webpack:prod']);
+    grunt.registerTask('index', 'string-replace:dist');
 
 };
 

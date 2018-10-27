100b43c3345352bd3242a5a0b023fa90722a3de1
Douglass Turner <douglass.turner@gmail.com>
2018-10-27 10:05:31 -0400

    dev

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -18,20 +18,18 @@ var igvwebConfig = {
             showChromosomeWidget: true,
 
             genome: "hg19",
+
             apiKey: "AIzaSyDUUAUFpQEN4mumeMNIRWXSiTh5cPtUAD0"
         },
 
-<<<<<<< HEAD
     // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
-    clientId: "CLIENT_ID",
-=======
     clientId: "661332306814-8nt29308rppg325bkq372vli8nm3na14.apps.googleusercontent.com",
->>>>>>> session load/save build out.
 
     // Provide a URL shorterner function or object.   This is optional.  If not supplied sharable URLs will not
     // be shortened but will be usable.
     urlShortener: {
         provider: "bitly",
+
         apiKey: "76670dc60b519eaf9be4fc1c227b4f3e3b3a5e26"
     }
 
@@ -40,5 +38,6 @@ var igvwebConfig = {
     //     apiKey: "API_KEY"
     // }
 
-}
+};
+
 

47e017b80c36b3d7023e4726344973febbf87c27
Jim Robinson <jrobinso@broadinstitute.org>
2019-01-11 21:40:54 -0800

    .

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -4,37 +4,40 @@
 
 "use strict";
 
-var igvwebConfig =
-    {
+var igvwebConfig = {
 
-        genomes: "resources/genomes.json",
+    genomes: "resources/genomes.json",
 
-        trackRegistryFile: "resources/tracks/trackRegistry.json",
+    trackRegistryFile: "resources/tracks/trackRegistry.json",
 
-        embedTarget: 'https://igv.org/web/release/2.2.0/embed.html',
+    embedTarget: 'https://igv.org/web/release/2.2.0/embed.html',
 
-        igvConfig:
-            {
-                queryParametersSupported: true,
-                showChromosomeWidget: true,
-                genome: "hg19",
-                apiKey: "AIzaSyDUUAUFpQEN4mumeMNIRWXSiTh5cPtUAD0"
-            },
+    igvConfig:
+        {
+            queryParametersSupported: true,
+            showChromosomeWidget: true,
 
-        // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
-        clientId: "661332306814-8nt29308rppg325bkq372vli8nm3na14.apps.googleusercontent.com",
+            genome: "hg19",
 
-        // Provide a URL shorterner function or object.   This is optional.  If not supplied sharable URLs will not
-        // be shortened but will be usable.
-        urlShortener:
-            {
-                provider: "bitly",
-                apiKey: "76670dc60b519eaf9be4fc1c227b4f3e3b3a5e26"
-            }
+            apiKey: "API_KEY"
+        },
 
-        // urlShortener: {
-        //     provider: "google",
-        //     apiKey: "API_KEY"
-        // }
+    // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
+    clientId: "CLIENT_ID",
 
-    };
+    // Provide a URL shorterner function or object.   This is optional.  If not supplied sharable URLs will not
+    // be shortened but will be usable.
+    urlShortener: {
+        provider: "bitly",
+
+        apiKey: "BITLY_TOKEN"
+    }
+
+    // urlShortener: {
+    //     provider: "google",
+    //     apiKey: "API_KEY"
+    // }
+
+};
+
+

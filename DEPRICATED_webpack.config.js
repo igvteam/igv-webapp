2c5699778cf5a79f44581bc3755832cc944fce63
Douglass Turner <douglass.turner@gmail.com>
2019-08-23 14:29:15 -0400

    webpack based buid

---
+++ DEPRICATED_webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports =
+    {
+        mode: 'none',
+        // entry: ['babel-polyfill', './build/js/app.js'],
+        entry: './build/js/app.js',
+        output:
+            {
+                path: path.resolve(__dirname, 'dist'),
+                filename: 'bundle.js'
+            },
+        module:
+            {
+                rules:
+                    [
+                        {
+                            test: /\.js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use:
+                                {
+                                    loader: 'babel-loader',
+                                    options:
+                                        {
+                                            presets:
+                                                [
+                                                    '@babel/preset-env'
+                                                ]
+                                        }
+                                }
+                        }
+                    ]
+            },
+        plugins:
+            [
+                new CopyWebpackPlugin([
+                    { from:'css/**/*.css'   },
+                    { from:'css/webfonts/*' },
+
+                    // { from:'css/app.css', to:'css' },
+
+                    { from:'img/*' },
+                    { from:'resources/*/*' },
+                    { from:'favicon.ico' }
+                ])
+
+            ]
+    };

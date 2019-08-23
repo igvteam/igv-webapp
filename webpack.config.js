af9e0b5712732a934effd9b12570627e1b6f5f11
Douglass Turner <douglass.turner@gmail.com>
2019-08-23 16:11:23 -0400

    webpack.config.js hacking

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const CopyPlugin = require('copy-webpack-plugin');
 process.env.INDEX_FILE_SRC = '<script type="module" src="js/app.js"></script>';
 process.env.INDEX_FILE_DST = '<script src="./app_bundle.js"></script>';
 
+const client_id_regex = /CLIENT_ID/gi;
+const api_key_regex = /API_KEY/gi;
+const bitly_token_regex = /BITLY_TOKEN/gi;
+
 module.exports =
     {
         mode: 'none',
@@ -45,15 +49,19 @@ module.exports =
                     { from:'resources/**/*'  },
                     { from:'vendor/*'        },
                     { from:'favicon.ico'     },
-                    { from:'igvwebConfig.js' },
                     {
-                        from: 'index.html',
+                        from: 'igvwebConfig.js',
                         transform: (content) => {
-                            return content
-                                .toString()
-                                .replace(process.env.INDEX_FILE_SRC, process.env.INDEX_FILE_DST);
+                            return content.toString()
+                                .replace(client_id_regex, '661332306814-8nt29308rppg325bkq372vli8nm3na14.apps.googleusercontent.com')
+                                .replace(api_key_regex, 'AIzaSyDUUAUFpQEN4mumeMNIRWXSiTh5cPtUAD0')
+                                .replace(bitly_token_regex, '76670dc60b519eaf9be4fc1c227b4f3e3b3a5e2')
                         }
                     },
+                    {
+                        from: 'index.html',
+                        transform: (content) => { return content.toString().replace(process.env.INDEX_FILE_SRC, process.env.INDEX_FILE_DST) }
+                    }
                 ])
 
             ]

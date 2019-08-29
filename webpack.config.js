853829d77be875abd0e5cf6784d0d3239fcefb9e
Douglass Turner <douglass.turner@gmail.com>
2019-08-29 09:24:47 -0400

    webpack'ing

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,11 @@ const CopyPlugin = require('copy-webpack-plugin');
 process.env.INDEX_FILE_SRC = '<script type="module" src="js/app.js"></script>';
 process.env.INDEX_FILE_DST = '<script src="./app_bundle.js"></script>';
 
-const client_id_regex = /CLIENT_ID/gi;
-const api_key_regex = /API_KEY/gi;
-const bitly_token_regex = /BITLY_TOKEN/gi;
-
 module.exports = env => {
 
-    const { production } = env;
-    const str = production || 'not_production';
+    const { igvwebconfig } = env;
 
-    console.log(`production ${ str }`);
+    console.log(`Using ${ igvwebconfig }`);
 
     return {
         mode: 'none',
@@ -49,20 +44,15 @@ module.exports = env => {
         plugins:
             [
                 new CopyPlugin([
-                    { from:'css/**/*.css'    },
-                    { from:'css/webfonts/*'  },
-                    { from:'img/*'           },
-                    { from:'resources/**/*'  },
-                    { from:'vendor/*'        },
-                    { from:'favicon.ico'     },
+                    { from:'css/**/*.css'   },
+                    { from:'css/webfonts/*' },
+                    { from:'img/*'          },
+                    { from:'resources/**/*' },
+                    { from:'vendor/*'       },
+                    { from:'favicon.ico'    },
                     {
-                        from: 'igvwebConfig.js',
-                        transform: (content) => {
-                            return content.toString()
-                                .replace(client_id_regex, '661332306814-8nt29308rppg325bkq372vli8nm3na14.apps.googleusercontent.com')
-                                .replace(api_key_regex, 'AIzaSyDUUAUFpQEN4mumeMNIRWXSiTh5cPtUAD0')
-                                .replace(bitly_token_regex, '76670dc60b519eaf9be4fc1c227b4f3e3b3a5e2')
-                        }
+                        from: igvwebconfig,
+                        to:'igvwebConfig.js'
                     },
                     {
                         from: 'index.html',

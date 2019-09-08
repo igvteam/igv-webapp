125234fce98ab361035d82a7a5d70ded06d13154
Jim Robinson <jrobinso@broadinstitute.org>
2019-09-07 21:11:05 -0700

    script for injecting api keys into igv web config

---
+++ injectKeys.js
@@ -0,0 +1,35 @@
+// script for injecting api keys into igv web config
+
+const fs = require('fs');
+
+const args = process.argv;
+let clientId;
+let apiKey;
+let bitlyToken;
+for (let a of args) {
+    if (a.startsWith("--clientId=")) {
+        clientId = a.substr(11);
+    } else if (a.startsWith("--apiKey=")) {
+        apiKey = a.substr(9);
+    } else if (a.startsWith("--bitlyToken=")) {
+        bitlyToken = a.substr(13);
+    }
+}
+console.log(process.argv);
+
+let contents = fs.readFileSync('igvwebConfig.js', 'utf-8');
+
+if (bitlyToken || apiKey || clientId) {
+    if (bitlyToken) {
+        contents = contents.replace('BITLY_TOKEN', bitlyToken);
+    }
+    if (apiKey) {
+        contents = contents.replace('API_KEY', apiKey);
+    }
+    if (clientId) {
+        contents = contents.replace("CLIENT_ID", clientId);
+    }
+
+    console.log(contents)
+    fs.writeFileSync('igvwebConfig.js', contents, 'utf-8');
+}

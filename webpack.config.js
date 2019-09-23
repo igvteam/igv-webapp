ffcdd606a7d77847e4e7ba9a04fdc1eb15fbcc94
Jim Robinson <jrobinso@broadinstitute.org>
2019-09-23 14:59:40 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,24 +22,15 @@ module.exports = env => {
                 filename: '[name].js'
             },
         module: {
-            rules:
-                [
-                    {
-                        test: /\.js$/,
-                        exclude: /(node_modules|bower_components)/,
-                        use:
-                            {
-                                loader: 'babel-loader',
-                                options:
-                                    {
-                                        presets:
-                                            [
-                                                '@babel/preset-env'
-                                            ]
-                                    }
-                            }
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: "babel-loader"
                     }
-                ]
+                }
+            ]
         },
         plugins:
             [

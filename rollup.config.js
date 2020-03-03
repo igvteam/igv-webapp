33f760cdd0edf7dd070d5f70b5aa41e8897d4e97
Jim Robinson <jrobinso@broadinstitute.org>
2020-03-02 22:34:11 -0800

    update build packages and script bump igv version

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import strip from 'rollup-plugin-strip';
+import commonjs from 'rollup-plugin-commonjs';
+import {terser} from "rollup-plugin-terser"
 
 export default [
 
     {
         input: 'js/app.js',
         output: [
-            {file: 'tmp/app_bundle.js', format: 'umd', name: 'igv_webapp'},
+            {file: 'dist/app_bundle.js', format: 'umd', name: 'igv_webapp'},
+            {file: 'dist/app_bundle.min.js', format: 'umd', name: 'igv_webapp', sourcemap: true}
         ],
         plugins: [
-            resolve(),
             strip({
                 // set this to `false` if you don't want to
                 // remove debugger statements
@@ -23,9 +25,15 @@ export default [
                 // defaults to `true`
                 sourceMap: false
             }),
-            babel({
-                exclude: 'node_modules/**'
-            }),
+            commonjs(),
+            resolve(),
+            babel(),
+            terser({
+                include: [/^.+\.min\.js$/],
+                sourcemap: {
+                    filename: "app_bundle.min.js",
+                    url: "app_bundle.min.js.map"
+                }})
         ]
     }
 ];
--- scripts/clean.js
+++ scripts/clean.js
@@ -1,5 +1,3 @@
 #!/usr/bin/env node
 const fs = require('fs-extra');
-
 fs.emptyDirSync(__dirname + '/../dist');
-fs.emptyDirSync(__dirname + '/../tmp');

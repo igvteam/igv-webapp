8d425993877412a6fff892aa88cb1ac3c4fabd74
Douglass Turner <douglass.turner@gmail.com>
2020-04-14 09:26:02 -0400

    update

--- js/multipleTrackFileLoad.js
+++ js/multipleTrackFileLoad.js
@@ -23,7 +23,6 @@
 
 import igv from '../node_modules/igv/dist/igv.esm.js';
 import {  Alert, GoogleFilePicker } from '../node_modules/igv-widgets/dist/igv-widgets.js';
-import * as app_google from './app-google.js';
 import { knownDataFileIndexFileLookup, getExtension, isValidIndexExtension } from './utils.js';
 const google = igv.google;
 

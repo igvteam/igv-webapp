66db95f0f701f2504fb5734ec8a794e3564bab33
Jim Robinson <jrobinso@broadinstitute.org>
2020-04-27 23:46:36 -0700

    add tracks template to igv config

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -1,24 +1,26 @@
 var igvwebConfig = {
-
     genomes: "resources/genomes.json",
-
     trackRegistryFile: "resources/tracks/trackRegistry.json",
-
-    embedTarget: 'https://igv.org/web/release/2.2.0/embed.html',
-
     igvConfig:
         {
             queryParametersSupported: true,
             showChromosomeWidget: true,
             genome: "hg19",
-            showSVGButton: false
+            showSVGButton: false,
+            tracks: [
+                // TODO -- add default tracks here.  See github.com/igvteam/igv.js/wiki for details
+                // {
+                //     name: "CTCF - string url",
+                //     type: "wig",
+                //     format: "bigwig",
+                //     url: "https://www.encodeproject.org/files/ENCFF563PAW/@@download/ENCFF563PAW.bigWig"
+                // }
+            ]
         },
 
-    // Provide a URL shorterner function or object.   This is optional.  If not supplied sharable URLs will not
-    // be shortened but will be usable.
+    // Provide a URL shorterner function or object.   This is optional.  If not supplied
+    // sharable URLs will not be shortened .
     urlShortener: {
         provider: "tinyURL"
     }
-
-
 };

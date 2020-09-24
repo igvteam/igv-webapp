a304e039e3f022b6986ab74bf767cb7425221f6d
Raoul Jean Pierre Bonnal <raoul.bonnal@ifom.eu>
2020-09-24 14:29:30 +0200

    Use custom config for CUSTOM track type; table definition and parser

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -1,3 +1,86 @@
+/*
+const customTrackDatasourceConfigurator = genomeId => {
+
+    return {
+        isJSON: false,
+        genomeId,
+        dataSetPath: undefined,
+        addIndexColumn: false,
+        columns:
+        [
+	    'id',
+            'library_id',
+	    'project',
+	    'project_alt',
+	    'group',
+	    'date',
+	    'indexes',
+	    'provider',
+	    'subject',
+	    'sampling_time',
+	    'amount_starting_cells',
+	    'tags',
+	    'notes',
+	    'scrna_seq_id',
+	    'scvdj_seq_id',
+	    'immunoprecipitate_anti_igg',
+	    'immunoprecipitate_input_dna',
+	    'immunoprecipitate',
+	    'subject_id',
+	    'created_at',
+	    'updated_at',
+	    'hospital',
+	    'protocol',
+	    'application',
+	    'operator',
+	    'organ_of_origin',
+	    'tissue_source_origin_area',
+	    'sample_type',
+	    'tissue',
+	    'disease',
+	    'disease_subtype',
+	    'cell',
+	    'culture',
+	    'datatype',
+            'HREF',         // hide
+            ],
+	hiddenColumns:
+        [
+            'HREF'
+        ],
+        parser: function(str, columnDictionary, addIndexColumn) {
+	    const keys = Object.keys(columnDictionary);
+	    let records = Papa.parse(str, {
+		delimiter: ",",
+		quoteChar: '"',
+		skipEmptyLines: true,
+		header: true
+	    }).data;
+	    records.forEach( row => {
+		row['HREF']=`${this.dataSetPathPrefix}${row['HREF']}`;
+	    });
+
+	    return records;
+	},
+        selectionHandler: selectionList => {
+            return selectionList.map(({ name, HREF }) => {
+                return { name, url: HREF }
+            })
+        }
+
+    }
+}
+
+var customWidgets = [
+	{id:'igvappcustommaporganoidsmodal',
+	 configurator: customTrackDatasourceConfigurator
+	},
+	{id:'igvappcustommapfightcancermodal',
+	 configurator: customTrackDatasourceConfigurator
+	}
+    ]
+
+*/
 var igvwebConfig = {
     genomes: "resources/genomes.json",
     trackRegistryFile: "resources/tracks/trackRegistry.json",
@@ -22,5 +105,6 @@ var igvwebConfig = {
     // sharable URLs will not be shortened .
     urlShortener: {
         provider: "tinyURL"
-    }
+    },
+    customWidgets: [] // or customWidgets defined above.
 };

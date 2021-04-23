7e22e1ad69763769c5f1707eeab2509cf948707d
jrobinso <jrobinso@broadinstitute.org>
2021-04-23 08:31:59 -0700

    update rollup modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
 import strip from 'rollup-plugin-strip';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from "@rollup/plugin-commonjs";
 import {terser} from "rollup-plugin-terser"
 const pkg = require('./package.json');
 
@@ -23,7 +23,7 @@ export default [
                 functions: ['console.log', 'assert.*', 'debug'],
             }),
             commonjs(),
-            resolve(),
+            nodeResolve(),
             babel()
         ]
     }

b7be309c6e7cd68869618da9591681bcf8814eb7
jrobinso <jrobinso@broadinstitute.org>
2021-11-22 14:42:19 -0800

    bump igv-widgets version for dynamic dropbox load

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -3,7 +3,8 @@ var igvwebConfig = {
     genomes: "resources/genomes.json",
     trackRegistryFile: "resources/tracks/trackRegistry.json",
 
-    // dropboxAPIKey: '8glijwyao9fq8we',
+    // Supply a drobpox api key to enable the Dropbox file picker in the load menus.  This is optional
+   // dropboxAPIKey: "...",
 
     // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
     //clientId: "...",

fc8d226ccc51b91cfce74089646290d1d9728cc3
Douglass Turner <douglass.turner@gmail.com>
2023-03-07 14:53:23 -0500

    google auth api foo

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -7,10 +7,6 @@ var igvwebConfig = {
     // Supply a drobpox api key to enable the Dropbox file picker in the load menus.  This is optional
     //dropboxAPIKey: "...",
 
-    // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
-    //clientId: "...",
-    // apiKey: "...",
-
     // Provide a URL shorterner function or object.   This is optional.  If not supplied
     // sharable URLs will not be shortened .
     urlShortener: {
---
+++ oauthConfig-private.js
@@ -0,0 +1,4 @@
+export default {
+    client_id: "661332306814-fmasnut050v7qds33tsa2rtvd5tc06sl.apps.googleusercontent.com",
+    apiKey: "AIzaSyCEmqU2lrAgKxJCbnJX87a5F3c9GejCCLA"
+}

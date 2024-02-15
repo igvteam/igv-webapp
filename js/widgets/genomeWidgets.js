02f0e045b339548908cbc7774264ac503b398775
jrobinso <933148+jrobinso@users.noreply.github.com>
2024-02-14 16:04:14 -0800

    Fix genark issue

--- js/widgets/genomeWidgets.js
+++ js/widgets/genomeWidgets.js
@@ -1,30 +1,7 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2016-2017 The Regents of the University of California
- * Author: Jim Robinson
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 
 import {ModalTable, GenericDataSource} from '../../node_modules/data-modal/src/index.js'
+import {StringUtils} from "../../node_modules/igv-utils/src/index.js"
+
 import Globals from "../globals.js"
 import AlertSingleton from "./alertSingleton.js"
 import {createURLModal} from "./urlModal.js"
@@ -48,10 +25,10 @@ function createGenomeWidgets({$igvMain, urlModalId, genarkModalId, genomeFileLoa
             selectionStyle: 'single',
             pageLength: 100,
             okHandler: result => {
-                const { accession } = result[ 0 ]
+                const {accession} = result[0]
                 loadGenome(accession)
             }
-        };
+        }
 
     genarkModalTable = new ModalTable(genarkModalTableConfig)
 
@@ -205,8 +182,15 @@ async function loadGenome(genomeConfiguration, custom = false) {
                 // Update the custom list
                 // hub.txt genomes are indirect, record name and id
                 if (!predefinedGenomeIds.has(g.id)) {
-                    if (!genomeConfiguration.id) genomeConfiguration.id = g.id
-                    if (!genomeConfiguration.name) genomeConfiguration.name = g.name
+                    if (StringUtils.isString(genomeConfiguration)) {
+                        genomeConfiguration = {id: genomeConfiguration}
+                    } else {
+                        if (!genomeConfiguration.id) genomeConfiguration.id = g.id
+                    }
+                    if (!genomeConfiguration.name) {
+                        genomeConfiguration.name = g.name
+                    }
+
 
                     const customGenomesString = localStorage.getItem("customGenomes")
                     let customGenomes = customGenomesString ? JSON.parse(customGenomesString) : []

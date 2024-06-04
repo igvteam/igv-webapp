887c8ad358874553c45ce071af97fb9a1b415391
turner <douglass.turner@gmail.com>
2024-06-04 08:35:24 -0400

    hacking

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -5,18 +5,17 @@ var igvwebConfig = {
     sessionRegistryFile: "resources/sessions/sessionRegistry.json",
 
     // Supply a drobpox api key to enable the Dropbox file picker in the load menus.  This is optional
-    //dropboxAPIKey: "...",
+    dropboxAPIKey: "8glijwyao9fq8we",
 
-    // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
-    //clientId: "...",
-    // apiKey: "...",
+     // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
+    clientId: "661332306814-fmasnut050v7qds33tsa2rtvd5tc06sl.apps.googleusercontent.com",
+    apiKey: "AIzaSyCEmqU2lrAgKxJCbnJX87a5F3c9GejCCLA",
 
     // Provide a URL shorterner function or object.   This is optional.  If not supplied
-    // sharable URLs will not be shortened.  If using tinyURL supply an api token
-    //urlShortener: {
-    //    provider: "tinyURL",
-    //    api_token: "..."
-    //},
+    // sharable URLs will not be shortened .
+    urlShortener: {
+        provider: "tinyURL"
+    },
 
     enableCircularView: true,
 

d3c5073fcc7955fd1211159625415b50c546948a
jrobinso <933148+jrobinso@users.noreply.github.com>
2025-05-07 22:59:37 -0700

    ...

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -5,10 +5,10 @@ const igvwebConfig = {
     sessionRegistryFile: "resources/sessions/sessionRegistry.json",
 
     // Supply a drobpox api key to enable the Dropbox file picker in the load menus.  This is optional
-    dropboxAPIKey: 'e43594OGdsaWp3eWFvOWZxOHdl',
+   // dropboxAPIKey: '....',
 
     // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
-    clientId: "Z_%%12NTkxNTk1NjcyNjAyLWNsYXNwaHB1b3Jqc3I1c2gyNHU2YWF0NGk1dnZydGU2LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29t",
+    //clientId: "...",
 
     //
     googleDriveEnabled: true,
@@ -16,10 +16,10 @@ const igvwebConfig = {
 
     // Provide a URL shorterner function or object.   This is optional.  If not supplied
     // sharable URLs will not be shortened.  If using tinyURL supply an api token
-    urlShortener: {
-        provider: "tinyURL",
-        api_token: "ll4539eTBiNHl3cm1ONGcwc0UwUmFqUkY5akNYOEUxVE9JRTEzSUdkREtYQW1BNnI3QnIyaVd3dHlvT1AxakxD"
-    },
+    //urlShortener: {
+   //    provider: "tinyURL",
+   //     api_token: "..."
+   // },
 
     enableCircularView: true,
 
